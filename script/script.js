61df82249b877cb09a23ca57bdbbb8be57baa972
weby82 <damien.bouvier@gmail.com>
2017-01-16 19:24:41 +0100

    clean du js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,4 @@
 	var charger= $('#charger');
-	// charge dans le tableau charger chaque element
-	// boutons.each(function(i, element){
-	var image = $(".img");
-	
 
 	var count = 0;
 	$(charger).on('click',function(){
